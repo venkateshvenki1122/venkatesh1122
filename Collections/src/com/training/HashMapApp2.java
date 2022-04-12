285528a4b828ae6afc6284c5ab036187c4219e2f
Venkatesh.T <Venkatesh.T@EXIND3010-09.allfon.in>
2022-04-12 12:42:03 +0530

    Encapsulation

---
+++ Collections/src/com/training/HashMapApp2.java
@@ -0,0 +1,59 @@
+package com.training;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.training.model.Student;
+
+public class HashMapApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student venki = new Student(101,"Venki",78);
+		
+		Student kawin = new Student(102, "Kawin", 90);
+		Student mukesh = new Student(103, "Mukesh", 90);
+		
+		HashMap<Integer,Student> map = new HashMap<>();
+		map.put(900, venki);
+		
+		map.put(902,kawin);
+		map.put(904, mukesh);
+		
+		System.out.println(map.get(900));
+		System.out.println(map.get(902));
+		System.out.println(map.get(904));
+		
+		Set<Integer> keys = map.keySet();
+		System.out.println(keys);
+		
+		for (Integer key : keys) {
+			
+			System.out.println(map.get(key));
+		}
+		System.out.println("*************************************");
+		
+		Collection<Student> value	=map.values();
+		
+		for (Student student : value) {
+			
+			System.out.println(student);
+		}
+		System.out.println("**************************************");
+		
+		Set<Map.Entry<Integer, Student>> list =map.entrySet();
+		
+		for (Entry<Integer, Student> entry : list) {
+			
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+		}
+		
+	}
+
+}
