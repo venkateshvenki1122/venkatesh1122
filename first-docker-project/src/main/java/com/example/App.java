c6bc422d597bf7a45ce17ac60d96439019bbbd2a
Venkatesh.T <Venkatesh.T@EXIND3010-09.allfon.in>
2022-08-23 11:04:00 +0530

    first-docker-project

---
+++ first-docker-project/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	List<String> names = Arrays.asList("Surya","Abishek","Sathya","Venki");
+    	
+    	names.forEach(System.out::println);
+    	
+    	
+    }
+}
